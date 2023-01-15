94b11a8a6dd2ecb0b5bb12168fae0bef988999d1
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-15 10:29:19 -0600

    worked on location window actions

--- scraper.py
+++ scraper.py
@@ -77,7 +77,7 @@ class Scraper():
 
         #Saves names of important elements as variables
         location_btn_class = 'x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x78zum5.x1a2a7pz.x1xmf6yo'
-
+        location_input_class = 'x1i10hfl.xggy1nq.x1s07b3s.x1kdt53j.x1a2a7pz.xjbqb8w.x76ihet.xwmqs3e.x112ta8.xxxdfa6.x9f619.xzsf02u.x1uxerd5.x1fcty0u.x132q4wb.x1a8lsjc.x1pi30zi.x1swvt13.x9desvi.xh8yej3.x15h3p50.x10emqs4'
 
         #Brings the driver to the facebook marketplace home if they are on a different page
         if 'https://www.facebook.com/marketplace/' not in str(self.driver.current_url):
@@ -90,8 +90,6 @@ class Scraper():
             alert.dismiss()
         except:
             pass
-
-
 
         #Waits for the driver to load the location button
         wait = WebDriverWait(self.driver, 10)
@@ -108,7 +106,19 @@ class Scraper():
         except:
             pass
 
-    
+        #The assumption now is that the scraper has opened the window for selecting a location.
+
+        #Waits for the driver to load the location input
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, location_input_class)))
+
+        #Clicks the input box for inputting a zip code
+        location_input = self.driver.find_element(By.CLASS_NAME, location_input_class)
+        actions.move_to_element(location_input).click(location_input).perform()
+
+        #Deletes the information in the box and replaces it with new zip code given by user
+        location_input.send_keys(Keys.DELETE) 
+        location_input.send_keys(self.get_zip()) #Put place informaion into these parentheses (must be taken as a parameter to input_location)
 
 x = Scraper()
 x.login()
